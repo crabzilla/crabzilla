2df8e888e4e435df574d54f94c375b787640381b
Rodolfo <rodolfo_dpk@yahoo.com>
2017-05-22 17:30:31 -0300

    moved database config to test module

--- crabzilla-tests/crabzilla-sql-stack-tests/src/test/java/crabzilla/stacks/sql/JdbiEventRepositoryIt.java
+++ crabzilla-tests/crabzilla-sql-stack-tests/src/test/java/crabzilla/stacks/sql/JdbiEventRepositoryIt.java
@@ -1,12 +1,19 @@
 package crabzilla.stacks.sql;
 
 import com.google.gson.Gson;
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
-import com.google.inject.Injector;
+import com.google.inject.TypeLiteral;
+import com.google.inject.name.Names;
+import com.google.inject.util.Modules;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import crabzilla.UnitOfWork;
 import crabzilla.Version;
 import crabzilla.example1.DatabaseModule;
+import crabzilla.example1.Example1SqlModule;
 import crabzilla.example1.aggregates.CustomerModule;
+import crabzilla.example1.aggregates.customer.Customer;
 import crabzilla.example1.aggregates.customer.CustomerId;
 import crabzilla.example1.aggregates.customer.commands.CreateCustomerCmd;
 import crabzilla.example1.aggregates.customer.events.CustomerCreated;
@@ -19,16 +26,14 @@ import org.skife.jdbi.v2.TransactionCallback;
 
 import javax.inject.Inject;
 import java.util.Arrays;
+import java.util.Properties;
 import java.util.UUID;
+import java.util.function.Function;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @DisplayName("A JdbiEventRepository")
 public class JdbiEventRepositoryIt {
-
-  final static Injector injector = Guice.createInjector(
-          new DatabaseModule(),
-          new CustomerModule());
 
   @Inject
   Gson gson;
@@ -39,7 +44,28 @@ public class JdbiEventRepositoryIt {
 
   @BeforeEach
   public void setup() {
-    injector.injectMembers(this);// TODO vai ter que voltar o example1-data-stack
+    Guice.createInjector(Modules.combine(
+            new DatabaseModule(),
+            new CustomerModule(),
+            new Example1SqlModule()), new AbstractModule() {
+
+      @Override
+      protected void configure() {
+
+        final Config config = ConfigFactory.load();
+        final Properties props =  new Properties();
+        config.entrySet().forEach(e -> {
+          final String key = e.getKey().replace("crabzilla-stack1.", "");
+          final String value = e.getValue().render().replace("\"", "");
+          props.put(key, value);
+        });
+        Names.bindProperties(binder(), props);
+
+        bind(new TypeLiteral<Function<Customer, Customer>>() {;}).toInstance(c -> c);
+
+      }
+    }).injectMembers(this);
+
     repo = new JdbiEventRepository("customer", gson, dbi);
     dbi.inTransaction((TransactionCallback<Void>) (handle, transactionStatus) -> {
       handle.execute("delete from idempotency");
