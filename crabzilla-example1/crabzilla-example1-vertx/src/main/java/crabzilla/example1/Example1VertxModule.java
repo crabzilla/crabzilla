da88aeb6979e0d0ccad8ccadb794fd3cd146c73f
Rodolfo <rodolfo_dpk@yahoo.com>
2017-05-24 02:29:45 -0300

    cleanup

--- crabzilla-example1/crabzilla-example1-vertx/src/main/java/crabzilla/example1/Example1VertxModule.java
+++ crabzilla-example1/crabzilla-example1-vertx/src/main/java/crabzilla/example1/Example1VertxModule.java
@@ -1,7 +1,9 @@
 package crabzilla.example1;
 
 import com.github.benmanes.caffeine.cache.Cache;
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
@@ -10,6 +12,15 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import crabzilla.example1.aggregates.CustomerModule;
 import crabzilla.example1.aggregates.customer.Customer;
+import crabzilla.example1.aggregates.customer.CustomerId;
+import crabzilla.example1.aggregates.customer.commands.ActivateCustomerCmd;
+import crabzilla.example1.aggregates.customer.commands.CreateActivateCustomerCmd;
+import crabzilla.example1.aggregates.customer.commands.CreateCustomerCmd;
+import crabzilla.example1.aggregates.customer.commands.DeactivateCustomerCmd;
+import crabzilla.example1.aggregates.customer.events.CustomerActivated;
+import crabzilla.example1.aggregates.customer.events.CustomerCreated;
+import crabzilla.example1.aggregates.customer.events.CustomerDeactivated;
+import crabzilla.example1.aggregates.customer.events.DeactivatedCmdScheduled;
 import crabzilla.example1.services.SampleService;
 import crabzilla.example1.services.SampleServiceImpl;
 import crabzilla.model.*;
@@ -19,9 +30,12 @@ import crabzilla.stacks.vertx.codecs.fst.AggregateRootIdCodec;
 import crabzilla.stacks.vertx.codecs.fst.CommandCodec;
 import crabzilla.stacks.vertx.codecs.fst.EventCodec;
 import crabzilla.stacks.vertx.codecs.fst.UnitOfWorkCodec;
+import crabzilla.stacks.vertx.gson.RuntimeTypeAdapterFactory;
 import crabzilla.stacks.vertx.sql.JdbiEventRepository;
 import crabzilla.stacks.vertx.verticles.CommandHandlerVerticle;
 import io.vertx.core.Vertx;
+import lombok.val;
+import net.dongliu.gson.GsonJava8TypeAdapterFactory;
 import org.nustaq.serialization.FSTConfiguration;
 import org.skife.jdbi.v2.DBI;
 
@@ -39,7 +53,6 @@ public class Example1VertxModule extends AbstractModule {
   protected void configure() {
 
     install(new CustomerModule());
-    install(new Example1SqlModule());
     install(new DatabaseModule());
 
     bind(SampleService.class).to(SampleServiceImpl.class).asEagerSingleton();
@@ -92,6 +105,43 @@ public class Example1VertxModule extends AbstractModule {
                                            Cache<String, Snapshot<Customer>> cache) {
     return
       new CommandHandlerVerticle<>(Customer.class, snapshotReaderFn, cmdHandler, validatorFn, eventStore, cache, vertx);
+  }
+
+  // gson
+
+
+  @Provides
+  @Singleton
+  Gson gson() {
+
+    RuntimeTypeAdapterFactory<AggregateRootId> rtaIds  = RuntimeTypeAdapterFactory.of(AggregateRootId.class)
+            .registerSubtype(CustomerId.class);
+
+    RuntimeTypeAdapterFactory<Command> rtaCommands  = RuntimeTypeAdapterFactory.of(Command.class)
+            .registerSubtype(CreateCustomerCmd.class)
+            .registerSubtype(ActivateCustomerCmd.class)
+            .registerSubtype(DeactivateCustomerCmd.class)
+            .registerSubtype(CreateActivateCustomerCmd.class);
+
+    RuntimeTypeAdapterFactory<Event> rtaEvents = RuntimeTypeAdapterFactory.of(Event.class)
+            .registerSubtype(CustomerCreated.class)
+            .registerSubtype(CustomerActivated.class)
+            .registerSubtype(CustomerDeactivated.class)
+            .registerSubtype(DeactivatedCmdScheduled.class);
+
+    val gsonBuilder = new GsonBuilder();
+
+    gsonBuilder.setPrettyPrinting();
+    gsonBuilder.registerTypeAdapterFactory(new GsonJava8TypeAdapterFactory());
+    gsonBuilder.registerTypeAdapterFactory(rtaIds);
+    gsonBuilder.registerTypeAdapterFactory(rtaCommands);
+    gsonBuilder.registerTypeAdapterFactory(rtaEvents);
+
+    gsonBuilder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES); // snake case
+
+    val gson = gsonBuilder.create();
+
+    return gson;
   }
 
 }
