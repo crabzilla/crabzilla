846d6ea87fa6dd568428772c3b336278a06e50ae
Rodolfo <rodolfo_dpk@yahoo.com>
2017-05-30 20:42:46 -0300

    EventRepository.append now returns the unit of work sequence

--- crabzilla-vertx-stack/src/main/java/crabzilla/stack/vertx/CommandHandlingResponse.java
+++ crabzilla-vertx-stack/src/main/java/crabzilla/stack/vertx/CommandHandlingResponse.java
@@ -27,10 +27,16 @@ public class CommandHandlingResponse {
   UUID commandId;
   RESULT result;
 
+  Long uowSequence;
   @Getter(AccessLevel.NONE)
   List<String> constraints;
   @Getter(AccessLevel.NONE)
   UnitOfWork unitOfWork;
+
+  public Optional<Long> getUowSequence() {
+    return RESULT.SUCCESS.equals(result) || RESULT.SUCCESS_PROJECTION_FALLBACK.equals(result) ?
+            Optional.of(uowSequence) : Optional.empty();
+  }
 
   public Optional<List<String>> getConstraints() {
     return RESULT.VALIDATION_ERROR.equals(result) ? Optional.of(constraints) : Optional.empty();
@@ -41,21 +47,23 @@ public class CommandHandlingResponse {
   }
 
   public static CommandHandlingResponse VALIDATION_ERROR(UUID commandId, List<String> constraints) {
-    return new CommandHandlingResponse(commandId, RESULT.VALIDATION_ERROR, constraints, null);
+    return new CommandHandlingResponse(commandId, RESULT.VALIDATION_ERROR, 0L, constraints, null);
   }
   public static CommandHandlingResponse FALLBACK(UUID commandId) {
-    return new CommandHandlingResponse(commandId, RESULT.FALLBACK, Collections.emptyList(), null);
+    return new CommandHandlingResponse(commandId, RESULT.FALLBACK, 0L, Collections.emptyList(), null);
 
   }  public static CommandHandlingResponse BUSINESS_ERROR(UUID commandId) {
-    return new CommandHandlingResponse(commandId, RESULT.BUSINESS_ERROR, Collections.emptyList(), null);
+    return new CommandHandlingResponse(commandId, RESULT.BUSINESS_ERROR, 0L, Collections.emptyList(), null);
 
   }  public static CommandHandlingResponse UNKNOWN_COMMAND(UUID commandId) {
-    return new CommandHandlingResponse(commandId, RESULT.UNKNOWN_COMMAND, Collections.emptyList(), null);
+    return new CommandHandlingResponse(commandId, RESULT.UNKNOWN_COMMAND, 0L, Collections.emptyList(), null);
   }
-  public static CommandHandlingResponse SUCCESS(UnitOfWork uow) {
-    return new CommandHandlingResponse(uow.getCommand().getCommandId(), RESULT.SUCCESS, Collections.emptyList(), uow);
+  public static CommandHandlingResponse SUCCESS(UnitOfWork uow, Long uowSequence) {
+    return new CommandHandlingResponse(uow.getCommand().getCommandId(), RESULT.SUCCESS, uowSequence,
+            Collections.emptyList(), uow);
   }
-  public static CommandHandlingResponse SUCCESS_PROJECTION_FALLBACK(UnitOfWork uow) {
-    return new CommandHandlingResponse(uow.getCommand().getCommandId(), RESULT.SUCCESS_PROJECTION_FALLBACK, Collections.emptyList(), uow);
+  public static CommandHandlingResponse SUCCESS_PROJECTION_FALLBACK(UnitOfWork uow, Long uowSequence) {
+    return new CommandHandlingResponse(uow.getCommand().getCommandId(), RESULT.SUCCESS_PROJECTION_FALLBACK,
+            uowSequence, Collections.emptyList(), uow);
   }
 }
