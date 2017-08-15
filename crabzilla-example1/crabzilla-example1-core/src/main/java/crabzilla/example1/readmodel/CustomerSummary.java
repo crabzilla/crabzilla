88c5f100af370021d35b7cf9720a3d3bd91d7177
Rodolfo <rodolfo_dpk@yahoo.com>
2017-08-15 00:42:44 -0300

    cleanup

--- crabzilla-example1/crabzilla-example1-core/src/main/java/crabzilla/example1/readmodel/CustomerSummary.java
+++ crabzilla-example1/crabzilla-example1-core/src/main/java/crabzilla/example1/readmodel/CustomerSummary.java
@@ -1,16 +1,15 @@
 package crabzilla.example1.readmodel;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class CustomerSummary {
 
   String id;
   String name;
   Boolean isActive;
-
-  public CustomerSummary() { // TODO @Data should create this, right?
-  }
 
   public CustomerSummary(String id, String name, boolean isActive) {
     this.id = id;
