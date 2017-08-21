bf9a13f3e76ba779c2cba6083ec6fc71334aa36c
rodolfodpk <rodolfo_dpk@yahoo.com>
2017-08-21 02:59:20 -0300

    workaround sonartype staging rules

---
+++ crabzilla-kotlin-example1/crabzilla-kotlin-example1-database/src/main/java/io/github/crabzilla/example1/Workaround.java
@@ -0,0 +1,8 @@
+package io.github.crabzilla.example1;
+
+/**
+ * This class purpose is just to allow deploy to maven central since Sonartype require javadocs and sources artifacst.
+ * It's only a workaround.
+ */
+public class Workaround {
+}
