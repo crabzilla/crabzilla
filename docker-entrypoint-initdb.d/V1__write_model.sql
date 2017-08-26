4b49c7d3901b273e3d93ff4faab333d496852385
rodolfodpk <rodolfo_dpk@yahoo.com>
2017-08-25 21:38:24 -0300

    docker entrypoint

---
+++ docker-entrypoint-initdb.d/V1__write_model.sql
@@ -0,0 +1,22 @@
+use example1db
+
+DROP TABLE if exists units_of_work ;
+
+CREATE TABLE units_of_work (
+      uow_seq_number BIGINT AUTO_INCREMENT,
+	  uow_id VARCHAR(36) NOT NULL,
+      uow_events JSON NOT NULL,
+      cmd_id VARCHAR(36) NOT NULL,
+      cmd_data JSON NOT NULL,
+      ar_name VARCHAR(36) NOT NULL,
+      ar_id VARCHAR(36) NOT NULL,
+      version NUMERIC,
+      inserted_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      PRIMARY KEY (uow_seq_number, ar_name),
+      UNIQUE (uow_id, ar_name),
+      UNIQUE (cmd_id, ar_name)
+    )
+    PARTITION BY KEY(ar_name)
+    ;
+
+CREATE INDEX idx_ar_id ON units_of_work (ar_id);
