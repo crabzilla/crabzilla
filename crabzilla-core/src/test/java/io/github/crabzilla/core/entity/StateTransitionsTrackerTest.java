31ba4d6992ba2884fc6843366975bed5f1bdc303
rodolfodpk <rodolfo_dpk@yahoo.com>
2017-12-27 02:29:41 -0200

    build.sh to copy example1 to test jar and core test passing

--- crabzilla-core/src/test/java/io/github/crabzilla/core/entity/StateTransitionsTrackerTest.java
+++ crabzilla-core/src/test/java/io/github/crabzilla/core/entity/StateTransitionsTrackerTest.java
@@ -1,152 +1,163 @@
-//package io.github.crabzilla.core.entity;
-//
-//import io.github.crabzilla.core.example1.customer.Customer;
-//import io.github.crabzilla.core.example1.services.SampleInternalService;
-//import io.github.crabzilla.core.example1.services.SampleInternalServiceImpl;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Nested;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.MockitoAnnotations;
-//
-//import java.time.Instant;
-//
-//import static io.github.crabzilla.core.example1.customer.CustomerData.*;
-//import static io.github.crabzilla.core.example1.customer.CustomerFunctions.StateTransitionFn;
-//import static java.util.Arrays.asList;
-//import static java.util.Collections.singletonList;
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//@DisplayName("A StateTransitionsTracker")
-//public class StateTransitionsTrackerTest {
-//
-//  StateTransitionsTracker<Customer> tracker;
-//
-//  final SampleInternalService service = new SampleInternalServiceImpl();
-//
-//  final Customer customer =
-//          new Customer(service, null, null, false, null);
-//
-//  final Snapshot<Customer> originalSnapshot = new Snapshot<>(customer, new Version(0));
-//
-//  @BeforeEach
-//  void instantiate() {
-//    MockitoAnnotations.initMocks(this);
-//  }
-//
-//  @Test
-//  public void can_be_instantiated() {
-//    new StateTransitionsTracker<>(originalSnapshot, new StateTransitionFn());
-//  }
-//
-//  @Nested
-//  @DisplayName("when new")
-//  public class WhenIsNew {
-//
-//    @BeforeEach
-//    void instantiate() {
-//      tracker = new StateTransitionsTracker<>(originalSnapshot,
-//              new StateTransitionFn());
-//    }
-//
-//    @Test
-//    void is_empty() {
-//      assertThat(tracker.isEmpty()).isTrue();
-//    }
-//
-//    @Test
-//    void has_empty_state() {
-//      assertThat(tracker.currentState()).isEqualTo(originalSnapshot.getInstance());
-//    }
-//
-//    @Nested
-//    @DisplayName("when adding a create customer event")
-//    public class WhenAddingNewEvent {
-//
-//      final CustomerId id = new CustomerId("c1");
-//      private CustomerCreated customerCreated = new CustomerCreated(id, "customer-1");
-//      private Customer expectedCustomer = new Customer(service, id, "customer-1", false, null);
-//
-//      @BeforeEach
-//      void apply_create_event() {
-//        tracker.applyEvents(c -> singletonList(customerCreated));
-//      }
-//
-//      @Test
-//      void has_new_state() {
-//        assertThat(tracker.currentState()).isEqualTo(expectedCustomer);
-//      }
-//
-//      @Test
-//      void has_only_create_event() {
-//        assertThat(tracker.collectEvents()).contains(customerCreated);
-//        assertThat(tracker.collectEvents().size()).isEqualTo(1);
-//      }
-//
-//      @Nested
-//      @DisplayName("when adding an activate customer event")
-//      public class WhenAddingActivateEvent {
-//
-//        private CustomerActivated customerActivated = new CustomerActivated("is ok", Instant.now());
-//        private Customer expectedCustomer = new Customer(service, id, "customer-1", true,
-//                customerActivated.getReason());
-//
-//        @BeforeEach
-//        void apply_activate_event() {
-//          tracker.applyEvents(c -> singletonList(customerActivated));
-//        }
-//
-//        @Test
-//        void has_new_state() {
-//          assertThat(tracker.currentState()).isEqualTo(expectedCustomer);
-//        }
-//
-//        @Test
-//        void has_both_create_and_activated_evenst() {
-//          assertThat(tracker.collectEvents().get(0)).isEqualTo(customerCreated);
-//          assertThat(tracker.collectEvents().get(1)).isEqualTo(customerActivated);
-//          assertThat(tracker.collectEvents().size()).isEqualTo(2);
-//        }
-//
-//      }
-//
-//    }
-//
-//  }
-//
-//  @Nested
-//  @DisplayName("when adding both create and activate events")
-//  public class WhenAddingCreateActivateEvent {
-//
-//    final String IS_OK = "is ok";
-//
-//    final CustomerId id = new CustomerId("c1");
-//    private CustomerCreated customerCreated = new CustomerCreated(id, "customer-1");
-//    private CustomerActivated customerActivated = new CustomerActivated(IS_OK, Instant.now());
-//    private Customer expectedCustomer = new Customer(service, id, "customer-1", true, IS_OK);
-//
-//    @BeforeEach
-//    void instantiate() {
-//      // given
-//      tracker = new StateTransitionsTracker<>(originalSnapshot, new StateTransitionFn());
-//      // when
-//      tracker.applyEvents(c -> asList(customerCreated, customerActivated));
-//    }
-//
-//    // then
-//
-//    @Test
-//    void has_new_state() {
-//      assertThat(tracker.currentState()).isEqualTo(expectedCustomer);
-//    }
-//
-//    @Test
-//    void has_both_event() {
-//      assertThat(tracker.collectEvents()).contains(customerCreated);
-//      assertThat(tracker.collectEvents()).contains(customerActivated);
-//      assertThat(tracker.collectEvents().size()).isEqualTo(2);
-//    }
-//
-//  }
-//
-//}
+package io.github.crabzilla.core.entity;
+
+import io.github.crabzilla.example1.SampleInternalService;
+import io.github.crabzilla.example1.customer.*;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("A StateTransitionsTracker")
+public class StateTransitionsTrackerTest {
+
+  StateTransitionsTracker<Customer> tracker;
+
+  final SampleInternalService service = new SampleInternalService() {
+    @NotNull
+    @Override
+    public UUID uuid() {
+      return UUID.randomUUID();
+    }
+
+    @NotNull
+    @Override
+    public Instant now() {
+      return Instant.now();
+    }
+  };
+
+  final Customer customer =
+          new Customer(null, null, false, null, service);
+
+  final Snapshot<Customer> originalSnapshot = new Snapshot<>(customer, new Version(0));
+
+  @BeforeEach
+  void instantiate() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  public void can_be_instantiated() {
+    new StateTransitionsTracker<>(originalSnapshot, new StateTransitionFn());
+  }
+
+  @Nested
+  @DisplayName("when new")
+  public class WhenIsNew {
+
+    @BeforeEach
+    void instantiate() {
+      tracker = new StateTransitionsTracker<>(originalSnapshot,
+              new StateTransitionFn());
+    }
+
+    @Test
+    void is_empty() {
+      assertThat(tracker.isEmpty()).isTrue();
+    }
+
+    @Test
+    void has_empty_state() {
+      assertThat(tracker.currentState()).isEqualTo(originalSnapshot.getInstance());
+    }
+
+    @Nested
+    @DisplayName("when adding a create customer event")
+    public class WhenAddingNewEvent {
+
+      final CustomerId id = new CustomerId("c1");
+      private CustomerCreated customerCreated = new CustomerCreated(id, "customer-1");
+      private Customer expectedCustomer = new Customer(id, "customer-1", false, null, service);
+
+      @BeforeEach
+      void apply_create_event() {
+        tracker.applyEvents(c -> singletonList(customerCreated));
+      }
+
+      @Test
+      void has_new_state() {
+        assertThat(tracker.currentState()).isEqualTo(expectedCustomer);
+      }
+
+      @Test
+      void has_only_create_event() {
+        assertThat(tracker.collectEvents()).contains(customerCreated);
+        assertThat(tracker.collectEvents().size()).isEqualTo(1);
+      }
+
+      @Nested
+      @DisplayName("when adding an activate customer event")
+      public class WhenAddingActivateEvent {
+
+        private CustomerActivated customerActivated = new CustomerActivated("is ok", Instant.now());
+        private Customer expectedCustomer = new Customer(id, "customer-1", true,
+                customerActivated.getReason(), service);
+
+        @BeforeEach
+        void apply_activate_event() {
+          tracker.applyEvents(c -> singletonList(customerActivated));
+        }
+
+        @Test
+        void has_new_state() {
+          assertThat(tracker.currentState()).isEqualTo(expectedCustomer);
+        }
+
+        @Test
+        void has_both_create_and_activated_evenst() {
+          assertThat(tracker.collectEvents().get(0)).isEqualTo(customerCreated);
+          assertThat(tracker.collectEvents().get(1)).isEqualTo(customerActivated);
+          assertThat(tracker.collectEvents().size()).isEqualTo(2);
+        }
+
+      }
+
+    }
+
+  }
+
+  @Nested
+  @DisplayName("when adding both create and activate events")
+  public class WhenAddingCreateActivateEvent {
+
+    final String IS_OK = "is ok";
+
+    final CustomerId id = new CustomerId("c1");
+    private CustomerCreated customerCreated = new CustomerCreated(id, "customer-1");
+    private CustomerActivated customerActivated = new CustomerActivated(IS_OK, Instant.now());
+    private Customer expectedCustomer = new Customer(id, "customer-1", true, IS_OK, service);
+
+    @BeforeEach
+    void instantiate() {
+      // given
+      tracker = new StateTransitionsTracker<>(originalSnapshot, new StateTransitionFn());
+      // when
+      tracker.applyEvents(c -> asList(customerCreated, customerActivated));
+    }
+
+    // then
+
+    @Test
+    void has_new_state() {
+      assertThat(tracker.currentState()).isEqualTo(expectedCustomer);
+    }
+
+    @Test
+    void has_both_event() {
+      assertThat(tracker.collectEvents()).contains(customerCreated);
+      assertThat(tracker.collectEvents()).contains(customerActivated);
+      assertThat(tracker.collectEvents().size()).isEqualTo(2);
+    }
+
+  }
+
+}
