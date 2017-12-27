afeac0c22144dc3d39cc2f76435897a3ed504a09
rodolfodpk <rodolfo_dpk@yahoo.com>
2017-12-27 12:44:35 -0200

    replaced build.sh with maven resources plugin

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf crabzilla-core/src/test/java/io/github/crabzilla/example1
-cp -r crabzilla-example1/crabzilla-example1-core/src/main/java/io/github/crabzilla/example1 crabzilla-core/src/test/java/io/github/crabzilla
