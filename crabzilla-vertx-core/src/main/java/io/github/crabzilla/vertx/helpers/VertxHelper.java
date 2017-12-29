713823e548234b0a76342a78a4f742e4de453b8b
rodolfodpk <rodolfo_dpk@yahoo.com>
2017-12-29 03:50:36 -0200

    cleanup

--- crabzilla-vertx-core/src/main/java/io/github/crabzilla/vertx/helpers/VertxHelper.java
+++ crabzilla-vertx-core/src/main/java/io/github/crabzilla/vertx/helpers/VertxHelper.java
@@ -1,6 +1,5 @@
 package io.github.crabzilla.vertx.helpers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
--- crabzilla-vertx-core/src/main/java/io/github/crabzilla/vertx/modules/CrabzillaModule.kt
+++ crabzilla-vertx-core/src/main/java/io/github/crabzilla/vertx/modules/CrabzillaModule.kt
@@ -1,22 +1,9 @@
 package io.github.crabzilla.vertx.modules
 
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.KotlinModule
-import com.fasterxml.jackson.module.paramnames.ParameterNamesModule
 import dagger.Module
 import dagger.Provides
-import io.github.crabzilla.core.DomainEvent
-import io.github.crabzilla.core.entity.EntityCommand
-import io.github.crabzilla.core.entity.EntityId
-import io.github.crabzilla.core.entity.EntityUnitOfWork
-import io.github.crabzilla.vertx.EntityCommandExecution
-import io.github.crabzilla.vertx.ProjectionData
-import io.github.crabzilla.vertx.codecs.JacksonGenericCodec
 import io.github.crabzilla.vertx.helpers.VertxHelper
 import io.vertx.core.Vertx
-import io.vertx.core.json.Json
 import io.vertx.core.json.JsonObject
 import javax.inject.Singleton
 
