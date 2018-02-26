bbea7735efa6556e3c227a5f0a87712a7a0c3856
rodolfodpk <rodolfo_dpk@yahoo.com>
2018-02-25 22:49:49 -0300

    modules hierarchy

--- crabzilla-example1/crabzilla-example1-handler/src/main/java/io/github/crabzilla/example1/Example1HandlerModule.kt
+++ crabzilla-example1/crabzilla-example1-handler/src/main/java/io/github/crabzilla/example1/Example1HandlerModule.kt
@@ -4,10 +4,6 @@ import dagger.Module
 import dagger.Provides
 import io.github.crabzilla.example1.customer.CustomerModule
 import io.github.crabzilla.example1.impl.SampleInternalServiceImpl
-import io.github.crabzilla.vertx.UnitOfWorkRepository
-import io.github.crabzilla.vertx.WriteDatabase
-import io.github.crabzilla.vertx.impl.UnitOfWorkRepositoryImpl
-import io.vertx.ext.jdbc.JDBCClient
 import javax.inject.Singleton
 
 
