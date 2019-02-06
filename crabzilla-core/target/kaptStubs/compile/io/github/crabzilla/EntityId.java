4f0af73e55003f1cd03eaca4ace7fca1d9343508
rodolfodpk <rodolfo_dpk@yahoo.com>
2019-02-06 00:14:58 -0200

    cleanup kapt Files

--- crabzilla-core/target/kaptStubs/compile/io/github/crabzilla/EntityId.java
+++
@@ -1,8 +0,0 @@
-package io.github.crabzilla;
-
-@kotlin.Metadata(mv = {1, 1, 13}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u0010\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\b\n\u0000\bg\u0018\u00002\u00020\u0001J\b\u0010\u0002\u001a\u00020\u0003H&\u00a8\u0006\u0004"}, d2 = {"Lio/github/crabzilla/EntityId;", "Ljava/io/Serializable;", "value", "", "crabzilla-core"})
-@com.fasterxml.jackson.annotation.JsonTypeInfo(use = com.fasterxml.jackson.annotation.JsonTypeInfo.Id.CLASS, include = com.fasterxml.jackson.annotation.JsonTypeInfo.As.PROPERTY, property = "@class")
-public abstract interface EntityId extends java.io.Serializable {
-    
-    public abstract int value();
-}
