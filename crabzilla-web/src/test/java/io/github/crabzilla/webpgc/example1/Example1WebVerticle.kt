99ac022c855768313fb270bc95e12eae5cb9441c
rodolfodpk <rodolfo_dpk@yahoo.com>
2019-06-08 13:33:17 -0300

    added webpgc to coverage-report

---
+++ crabzilla-web/src/test/java/io/github/crabzilla/webpgc/example1/Example1WebVerticle.kt
@@ -0,0 +1,107 @@
+package io.github.crabzilla.webpgc.example1
+
+import io.github.crabzilla.EventBusUowPublisher
+import io.github.crabzilla.example1.aggregate.CustomerCommandAware
+import io.github.crabzilla.example1.aggregate.CustomerJsonAware
+import io.github.crabzilla.pgc.PgcEntityComponent
+import io.github.crabzilla.pgc.pgPool
+import io.github.crabzilla.web.WebEntityComponentImpl
+import io.reactiverse.pgclient.PgPool
+import io.vertx.config.ConfigRetriever
+import io.vertx.config.ConfigRetrieverOptions
+import io.vertx.config.ConfigStoreOptions
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.Future
+import io.vertx.core.Launcher
+import io.vertx.core.http.HttpServer
+import io.vertx.core.http.HttpServerOptions
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.handler.BodyHandler
+import io.vertx.ext.web.handler.LoggerHandler
+import org.slf4j.LoggerFactory.getLogger
+
+// Convenience method so you can run it in your IDE
+fun main() {
+  Launcher.executeCommand("run", Example1WebVerticle::class.java.name)
+}
+
+class Example1WebVerticle(val httpPort: Int = 8081, val configFile: String = "./example1.env") : AbstractVerticle() {
+
+  lateinit var server: HttpServer
+  lateinit var writeDb: PgPool
+
+  companion object {
+    internal var log = getLogger(Example1WebVerticle::class.java)
+  }
+
+  override fun start(startFuture: Future<Void>) {
+
+    log.info("*** starting using HTTP_PORT $httpPort")
+
+    val envOptions = ConfigStoreOptions()
+      .setType("file")
+      .setFormat("properties")
+      .setConfig(JsonObject().put("path", configFile))
+
+    val options = ConfigRetrieverOptions().addStore(envOptions)
+
+    val retriever = ConfigRetriever.create(vertx, options)
+
+    retriever.getConfig { configFuture ->
+      if (configFuture.failed()) {
+        log.error("Failed to get configuration", configFuture.cause())
+        startFuture.fail(configFuture.cause())
+        return@getConfig
+      }
+
+      val config = configFuture.result()
+
+      log.trace(config.encodePrettily())
+
+      // web
+
+      val router = Router.router(vertx)
+
+      router.route().handler(LoggerHandler.create())
+      router.route().handler(BodyHandler.create())
+
+      // example1
+      this.writeDb = pgPool(vertx, "WRITE", config)
+
+      val ec = PgcEntityComponent(writeDb, "customer", CustomerJsonAware(), CustomerCommandAware(),
+        EventBusUowPublisher(vertx, "example1"))
+
+      WebEntityComponentImpl(ec, "customers", router).deployWebRoutes()
+
+      // read model routes
+      router.get("/customers/:id").handler {
+          it.response()
+            .putHeader("Content-type", "application/json")
+            .end(JsonObject().put("message", "TODO query read model").encode())
+      }
+
+      server = vertx.createHttpServer(HttpServerOptions().setPort(httpPort).setHost("0.0.0.0"))
+
+      server.requestHandler(router).listen { startedFuture ->
+        if (startedFuture.succeeded()) {
+          log.info("Server started on port " + startedFuture.result().actualPort())
+          startFuture.complete()
+        } else {
+          log.error("oops, something went wrong during server initialization", startedFuture.cause())
+          startFuture.fail(startedFuture.cause())
+        }
+      }
+
+    }
+
+  }
+
+  override fun stop() {
+    log.info("*** closing resources")
+    writeDb.close()
+    server.close()
+  }
+
+}
+
---
+++ crabzilla-web/src/test/java/io/github/crabzilla/webpgc/example1/WebEntityComponentImpl.java
@@ -0,0 +1,220 @@
+package io.github.crabzilla.webpgc.example1;
+
+import io.github.crabzilla.example1.CreateCustomer;
+import io.github.crabzilla.example1.CustomerId;
+import io.github.crabzilla.example1.UnknownCommand;
+import io.github.crabzilla.web.ContentTypes;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.SLF4JLogDelegateFactory;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.ext.web.client.predicate.ResponsePredicate;
+import io.vertx.ext.web.codec.BodyCodec;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.ServerSocket;
+import java.util.Random;
+
+import static io.github.crabzilla.UnitOfWork.JsonMetadata.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Disabled // TODO
+@ExtendWith(VertxExtension.class)
+class WebEntityComponentImpl {
+
+  static {
+    System.setProperty(io.vertx.core.logging.LoggerFactory.LOGGER_DELEGATE_FACTORY_CLASS_NAME,
+      SLF4JLogDelegateFactory.class.getName());
+    LoggerFactory.getLogger(io.vertx.core.logging.LoggerFactory.class);// Required for Logback to work in Vertx
+  }
+
+  static Example1WebVerticle verticle;
+  static WebClient client;
+  static int port;
+
+  static final Random random = new Random();
+  static int nextInt = random.nextInt();
+  static int customerId2 = random.nextInt();
+  static final Logger log = LoggerFactory.getLogger(WebEntityComponentImpl.class);
+
+  private static int httpPort() {
+    int httpPort = 0;
+    try {
+      ServerSocket socket = new ServerSocket(0);
+      httpPort = socket.getLocalPort();
+      socket.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return httpPort;
+  }
+
+  @BeforeAll
+  static void setup(VertxTestContext tc, Vertx vertx) {
+    port = httpPort();
+    verticle = new Example1WebVerticle(port, "../example1.env");
+    log.info("will try to deploy MainVerticle using HTTP_PORT = " + port);
+    WebClientOptions wco = new WebClientOptions();
+    client = WebClient.create(vertx, wco);
+
+    vertx.deployVerticle(verticle, deploy -> {
+      if (deploy.succeeded()) {
+        verticle.writeDb.query("delete from units_of_work", event1 -> {
+          if (event1.failed()) {
+            tc.failNow(event1.cause());
+            return;
+          }
+          verticle.writeDb.query("delete from customer_snapshots", event2 -> {
+            if (event2.failed()) {
+              tc.failNow(event2.cause());
+              return;
+            }
+            tc.completeNow();
+          });
+        });
+      } else {
+        deploy.cause().printStackTrace();
+        tc.failNow(deploy.cause());
+      }
+    });
+  }
+
+  @Nested
+  @DisplayName("When sending a valid CreateCommand expecting uow body")
+  class When1 {
+
+    @Test
+    @DisplayName("You get a correspondent UnitOfWork as JSON")
+    void a1(VertxTestContext tc) {
+      CreateCustomer cmd = new CreateCustomer("customer#" + customerId2);
+      JsonObject cmdAsJson = JsonObject.mapFrom(cmd);
+      client.post(port, "0.0.0.0", "/customers/" + customerId2 + "/commands/" + "create"  )
+        .as(BodyCodec.jsonObject())
+        .expect(ResponsePredicate.SC_SUCCESS)
+        .expect(ResponsePredicate.JSON)
+        .sendJson(cmdAsJson, tc.succeeding(response1 -> tc.verify(() -> {
+            JsonObject uow = response1.body();
+            Long uowId = Long.valueOf(response1.getHeader("uowId"));
+            assertThat(uowId).isPositive();
+            System.out.println(uowId);
+            System.out.println(uow.encodePrettily());
+            assertThat(uow.getString(ENTITY_NAME)).isEqualTo("customer");
+            assertThat(uow.getInteger(ENTITY_ID)).isEqualTo(customerId2);
+            assertThat(uow.getString(COMMAND_ID)).isNotNull();
+            assertThat(uow.getString(COMMAND_NAME)).isEqualTo("create");
+            assertThat(uow.getJsonObject(COMMAND)).isEqualTo(cmdAsJson);
+            assertThat(uow.getInteger(VERSION)).isEqualTo(1);
+            assertThat(uow.getJsonArray(EVENTS).size()).isEqualTo(1);
+            tc.completeNow();
+        }))
+      );
+    }
+
+    @Test
+    @DisplayName("You get a correspondent snapshot (write model)")
+    void a2(VertxTestContext tc) {
+      client.get(port, "0.0.0.0", "/customers/" + customerId2)
+        .as(BodyCodec.jsonObject())
+        .expect(ResponsePredicate.SC_SUCCESS)
+        .expect(ResponsePredicate.JSON)
+        .putHeader("accept", ContentTypes.ENTITY_WRITE_MODEL)
+        .send(tc.succeeding(response2 -> tc.verify(() -> {
+          JsonObject jo = response2.body();
+          System.out.println(jo.encodePrettily());
+          assertThat(jo.getInteger("version")).isEqualTo(1);
+          JsonObject state = jo.getJsonObject("state");
+          assertThat(state.getInteger("customerId")).isEqualTo(customerId2);
+          assertThat(state.getString("name")).isEqualTo("customer#" + customerId2);
+          assertThat(state.getBoolean("isActive")).isFalse();
+          tc.completeNow();
+        })));
+    }
+
+    @Test
+    @DisplayName("You get a correspondent company summary (read model)")
+    void a22(VertxTestContext tc) {
+      client.get(port, "0.0.0.0", "/customers/" + customerId2)
+        .as(BodyCodec.jsonObject())
+        .expect(ResponsePredicate.SC_SUCCESS)
+        .expect(ResponsePredicate.JSON)
+        .send(tc.succeeding(response2 -> tc.verify(() -> {
+          JsonObject jo = response2.body();
+          System.out.println(jo.encodePrettily());
+          assertThat(jo.getString("message")).isEqualTo("TODO query read model");
+          tc.completeNow();
+        })));
+    }
+
+
+    @Test
+    @Disabled
+    @DisplayName("You get a correspondent entity tracking") // TODO
+    void a3(VertxTestContext tc) {
+      client.get(port, "0.0.0.0", "/customers/" + customerId2)
+        .as(BodyCodec.jsonObject())
+        .expect(ResponsePredicate.SC_SUCCESS)
+        .expect(ResponsePredicate.JSON)
+        .send(tc.succeeding(response2 -> tc.verify(() -> {
+          JsonObject jo = response2.body();
+          System.out.println(jo.encodePrettily());
+          JsonObject snapshot = jo.getJsonObject("snapshot");
+          assertThat(snapshot.getInteger("version")).isEqualTo(1);
+          JsonObject state = snapshot.getJsonObject("state");
+          assertThat(state.getInteger("customerId")).isEqualTo(customerId2);
+          assertThat(state.getString("name")).isEqualTo("customer#" + customerId2);
+          assertThat(state.getBoolean("isActive")).isFalse();
+          assertThat(jo.getJsonArray("units_of_work").size()).isEqualTo(1);
+          tc.completeNow();
+        })));
+    }
+
+  }
+
+  @Test
+  @DisplayName("When sending an invalid CreateCommand")
+  void a3(VertxTestContext tc) {
+    JsonObject invalidCommand = new JsonObject();
+    client.post(port, "0.0.0.0", "/customers/1/commands/" + "create")
+      .as(BodyCodec.none())
+      .expect(ResponsePredicate.SC_BAD_REQUEST)
+      .sendJson(invalidCommand, tc.succeeding(response -> tc.verify(() -> {
+          tc.completeNow();
+        }))
+      );
+  }
+
+  @Test
+  @DisplayName("When sending an invalid CreateCommand expecting uow id")
+  void a4(VertxTestContext tc) {
+    CreateCustomer cmd = new CreateCustomer("a bad name");
+    JsonObject jo = JsonObject.mapFrom(cmd);
+    client.post(port, "0.0.0.0", "/customers/" + nextInt + "/commands/" + "create")
+      .as(BodyCodec.none())
+      .expect(ResponsePredicate.SC_BAD_REQUEST)
+      .sendJson(jo, tc.succeeding(response -> tc.verify(() -> {
+          tc.completeNow();
+        }))
+      );
+  }
+
+  @Test
+  @DisplayName("When sending an UnknownCommand")
+  void a5(VertxTestContext tc) {
+    UnknownCommand cmd = new UnknownCommand(new CustomerId(nextInt));
+    JsonObject jo = JsonObject.mapFrom(cmd);
+    client.post(port, "0.0.0.0", "/customers/" + nextInt + "/commands/unknown")
+      .as(BodyCodec.none())
+      .expect(ResponsePredicate.SC_BAD_REQUEST)
+      .sendJson(jo, tc.succeeding(response -> tc.verify(() -> {
+          tc.completeNow();
+        }))
+      );
+  }
+
+}
