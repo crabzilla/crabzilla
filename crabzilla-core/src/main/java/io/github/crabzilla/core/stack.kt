3aa9d50166f7a905db47590f67ef372121882c9c
rodolfodpk <rodolfo_dpk@yahoo.com>
2021-03-12 14:58:17 -0300

    removing callback hell boilerplate

--- crabzilla-core/src/main/java/io/github/crabzilla/core/stack.kt
+++ crabzilla-core/src/main/java/io/github/crabzilla/core/stack.kt
@@ -100,56 +100,31 @@ class DefaultSnapshotRepo<A : AggregateRoot, C : Command, E : DomainEvent>(
 
   override fun upsert(id: Int, snapshot: Snapshot<A>): Future<Void> {
     val promise = Promise.promise<Void>()
-    sharedData.getAsyncMap<Int, String>(entityName) { event1 ->
-      if (event1.failed()) {
-        log.error("Failed to get map $entityName")
-        promise.fail(event1.cause())
-        return@getAsyncMap
-      }
-      val stateAsJson = JsonObject(json.encodeToString(AGGREGATE_ROOT_SERIALIZER, snapshot.state))
-      val mapEntryAsJson = JsonObject().put("version", snapshot.version).put("state", stateAsJson)
-      event1.result().put(id, mapEntryAsJson.encode()) { event2 ->
-        if (event2.failed()) {
-          log.error("Failed to put $id on map $entityName")
-          promise.fail(event2.cause())
-          return@put
-        }
-        promise.complete()
-      }
-    }
+    val stateAsJson = JsonObject(json.encodeToString(AGGREGATE_ROOT_SERIALIZER, snapshot.state))
+    val mapEntryAsJson = JsonObject().put("version", snapshot.version).put("state", stateAsJson)
+    sharedData.getAsyncMap<Int, String>(entityName)
+      .compose { map -> map.put(id, mapEntryAsJson.encode()) }
+      .onFailure { promise.fail(it.cause) }
+      .onSuccess { promise.complete() }
     return promise.future()
   }
 
   override fun get(id: Int): Future<Snapshot<A>?> {
     val promise = Promise.promise<Snapshot<A>>()
-    val defaultSnapshot = null
-    sharedData.getAsyncMap<Int, String>(entityName) { event1 ->
-      if (event1.failed()) {
-        log.error("Failed get map $entityName")
-        promise.fail(event1.cause())
-        return@getAsyncMap
-      }
-      event1.result().get(id) { event2 ->
-        if (event2.failed()) {
-          log.error("Failed to get $id on map $entityName")
-          promise.fail(event2.cause())
-          return@get
+    sharedData.getAsyncMap<Int, String>(entityName)
+      .compose { map -> map.get(id) }
+      .onFailure { promise.fail(it.cause) }
+      .onSuccess { snapshotJson ->
+        if (snapshotJson == null) {
+          promise.complete(null)
+        } else {
+          val mapEntryAsJson = JsonObject(snapshotJson)
+          val version = mapEntryAsJson.getInteger("version")
+          val stateAsJson = mapEntryAsJson.getJsonObject("state")
+          val state = json.decodeFromString(AGGREGATE_ROOT_SERIALIZER, stateAsJson.encode()) as A
+          promise.complete(Snapshot(state, version))
         }
-        val result = event2.result()
-        if (result == null) {
-          if (log.isDebugEnabled) {
-            log.debug("Returning default snapshot for $id on map $entityName")
-          }
-          promise.complete(defaultSnapshot)
-          return@get
-        }
-        val mapEntryAsJson = JsonObject(event2.result())
-        val version = mapEntryAsJson.getInteger("version")
-        val stateAsJson = mapEntryAsJson.getJsonObject("state")
-        val state = json.decodeFromString(AGGREGATE_ROOT_SERIALIZER, stateAsJson.encode()) as A
-        promise.complete(Snapshot(state, version))
       }
-    }
     return promise.future()
   }
 }
