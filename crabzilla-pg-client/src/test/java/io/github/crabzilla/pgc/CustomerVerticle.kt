47dde08398ced7c2c0de9d44b94279edcac3cb42
rodolfodpk <rodolfo_dpk@yahoo.com>
2021-04-09 16:34:03 -0300

    disabling read model projection

--- crabzilla-pg-client/src/test/java/io/github/crabzilla/pgc/CustomerVerticle.kt
+++ crabzilla-pg-client/src/test/java/io/github/crabzilla/pgc/CustomerVerticle.kt
@@ -1,5 +1,7 @@
 package io.github.crabzilla.pgc
 
+import io.github.crabzilla.example1.CustomerRepository
+import io.github.crabzilla.example1.customerJson
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Promise
 import io.vertx.pgclient.PgPool
@@ -28,32 +30,32 @@ class CustomerVerticle(private val defaultInterval: Long) : AbstractVerticle() {
           .onSuccess {
             log.info("Success")
             //promise.complete()
-            return@onSuccess
-//            val publisherVerticle = PgcPoolingProjectionVerticle(
-//              "customers", writeDb,
-//              EventBusEventsPublisher(topic, vertx.eventBus()), defaultInterval
-//            )
-//            val projectorVerticle = CustomerProjectorVerticle(customerJson, CustomerRepository(readDb))
-//            log.info("Will deploy publisherVerticle")
-//            vertx.deployVerticle(publisherVerticle)
-//              .onFailure { err ->
-//                log.error("When deploying publisherVerticle", it)
-//                promise.fail(err)
-//              }
-//              .onSuccess {
-//                log.info("publisherVerticle started")
-//                promise.complete()
-//                log.info("Now will deploy projectorVerticle")
-//                vertx.deployVerticle(projectorVerticle)
-//                  .onFailure {
-//                    log.error("When deploying projectorVerticle", it)
-//                    promise.fail(it)
-//                  }
-//                  .onSuccess {
-//                    log.info("projectorVerticle started")
-//                    promise.complete()
-//                  }
-//              }
+//            return@onSuccess
+            val publisherVerticle = PgcPoolingProjectionVerticle(
+              "customers", writeDb,
+              EventBusEventsPublisher(topic, vertx.eventBus()), defaultInterval
+            )
+            val projectorVerticle = CustomerProjectorVerticle(customerJson, CustomerRepository(readDb))
+            log.info("Will deploy publisherVerticle")
+            vertx.deployVerticle(publisherVerticle)
+              .onFailure { err ->
+                log.error("When deploying publisherVerticle", it)
+                // promise.fail(err)
+              }
+              .onSuccess {
+                log.info("publisherVerticle started")
+                // promise.complete()
+                log.info("Now will deploy projectorVerticle")
+                vertx.deployVerticle(projectorVerticle)
+                  .onFailure {
+                    log.error("When deploying projectorVerticle", it)
+                    //promise.fail(it)
+                  }
+                  .onSuccess {
+                    log.info("projectorVerticle started")
+                    //promise.complete()
+                  }
+              }
           }
       }
   }
