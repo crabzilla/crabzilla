bd83a2ece65e4fa61abfb52498879c2acd9f7132
rodolfodpk <rodolfo_dpk@yahoo.com>
2021-04-13 00:00:12 -0300

    moved EventsScanner.kt to abstract-stack.kt

--- crabzilla-stack/src/main/java/io/github/crabzilla/stack/EventsScanner.kt
+++
@@ -1,9 +0,0 @@
-package io.github.crabzilla.stack
-
-import io.vertx.core.Future
-
-interface EventsScanner {
-  fun streamName(): String
-  fun scanPendingEvents(numberOfRows: Int): Future<List<EventRecord>>
-  fun updateOffSet(eventId: Long): Future<Void>
-}
