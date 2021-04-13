c95bf405e701db7778692c0703297b7ef3bc20fb
rodolfodpk <rodolfo_dpk@yahoo.com>
2021-04-13 00:10:16 -0300

    extracted CommandMetadata.kt

--- crabzilla-stack/src/main/java/io/github/crabzilla/stack/stack.kt
+++ crabzilla-stack/src/main/java/io/github/crabzilla/stack/stack.kt
@@ -1,7 +1,5 @@
 package io.github.crabzilla.stack
 
-import java.util.UUID
-
 /**
  * An exception informing an concurrency violation
  */
@@ -9,12 +7,3 @@ class OptimisticConcurrencyConflict(message: String) : IllegalStateException(mes
 
 data class ValidationException(val errors: List<String>) : RuntimeException(errors.toString())
 
-/**
- * The client must knows how to instantiate it.
- */
-data class CommandMetadata(
-  val aggregateRootId: Int,
-  val id: UUID = UUID.randomUUID(),
-  val causationId: UUID = id,
-  val correlationID: UUID = id
-)
