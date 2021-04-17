4ad7dc799f264a2b70a8879a6ada3dd7e9ca0a00
rodolfodpk <rodolfo_dpk@yahoo.com>
2021-04-16 21:15:41 -0300

    snapshot consistency

--- crabzilla-pg-client/src/main/java/io/github/crabzilla/pgc/PgcSnapshotRepo.kt
+++ crabzilla-pg-client/src/main/java/io/github/crabzilla/pgc/PgcSnapshotRepo.kt
@@ -28,13 +28,13 @@ class PgcSnapshotRepo<A : AggregateRoot, C : Command, E : DomainEvent>(
 
   companion object {
     private val log = LoggerFactory.getLogger(PgcSnapshotRepo::class.java)
-    private const val SELECT_EVENTS_VERSION_AFTER_VERSION =
-      "SELECT event_payload, version " +
-        "FROM events " +
-        "WHERE ar_id = $1 " +
-        "AND ar_name = $2 " +
-        "AND version > $3 " +
-        "ORDER BY event_id "
+//    private const val SELECT_EVENTS_VERSION_AFTER_VERSION =
+//      "SELECT event_payload, version " +
+//        "FROM events " +
+//        "WHERE ar_id = $1 " +
+//        "AND ar_name = $2 " +
+//        "AND version > $3 " +
+//        "ORDER BY event_id "
     private const val ROWS_PER_TIME = 1000
   }
 
@@ -90,61 +90,61 @@ class PgcSnapshotRepo<A : AggregateRoot, C : Command, E : DomainEvent>(
       return promise.future()
     }
 
-    fun newSnapshot(conn: SqlConnection, snapshot: Snapshot<A>?): Future<Snapshot<A>?> {
-      val promise = Promise.promise<Snapshot<A>>()
-      val events = mutableListOf<E>()
-      var currentVersion = 0
-      conn.prepare(SELECT_EVENTS_VERSION_AFTER_VERSION) { ar0 ->
-        if (ar0.failed()) {
-          promise.fail(ar0.cause())
-          return@prepare
-        }
-        val pq: PreparedStatement = ar0.result()
-        // Streams require to run within a transaction
-        conn.begin { ar1 ->
-          if (ar1.failed()) {
-            log.error("When starting transaction", ar1.cause())
-            promise.fail(ar1.cause())
-          } else {
-            val tx: Transaction = ar1.result()
-            // Fetch ROWS_PER_TIME
-            val stream =
-              pq.createStream(ROWS_PER_TIME, Tuple.of(id, config.name.value, snapshot?.version ?: 0))
-            // Use the stream
-            stream.exceptionHandler { err -> log.error("Stream error", err) }
-            stream.handler { row ->
-              val jsonObject: JsonObject = row.get(JsonObject::class.java, 0)
-              val event: DomainEvent = config.json.decodeFromString(jsonObject.encode())
-              currentVersion = row.getInteger(1)
-              events.add(event as E)
-              if (log.isDebugEnabled) log.debug("Event: $event version: $currentVersion")
-            }
-            stream.endHandler {
-              if (log.isDebugEnabled) log.debug("End of stream")
-              // Attempt to commit the transaction
-              tx.commit { ar ->
-                if (ar.failed()) {
-                  log.error("tx.commit", ar.cause())
-                  promise.fail(ar.cause())
-                } else {
-                  if (log.isDebugEnabled) log.debug("tx.commit successfully")
-                  if (events.size == 0) {
-                    promise.complete(snapshot)
-                  } else {
-                    val currentInstance = events.fold(
-                      snapshot?.state,
-                      { state, event -> config.eventHandler.handleEvent(state, event) }
-                    )
-                    promise.complete(Snapshot(currentInstance!!, currentVersion))
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-      return promise.future()
-    }
+//    fun newSnapshot(conn: SqlConnection, snapshot: Snapshot<A>?): Future<Snapshot<A>?> {
+//      val promise = Promise.promise<Snapshot<A>>()
+//      val events = mutableListOf<E>()
+//      var currentVersion = 0
+//      conn.prepare(SELECT_EVENTS_VERSION_AFTER_VERSION) { ar0 ->
+//        if (ar0.failed()) {
+//          promise.fail(ar0.cause())
+//          return@prepare
+//        }
+//        val pq: PreparedStatement = ar0.result()
+//        // Streams require to run within a transaction
+//        conn.begin { ar1 ->
+//          if (ar1.failed()) {
+//            log.error("When starting transaction", ar1.cause())
+//            promise.fail(ar1.cause())
+//          } else {
+//            val tx: Transaction = ar1.result()
+//            // Fetch ROWS_PER_TIME
+//            val stream =
+//              pq.createStream(ROWS_PER_TIME, Tuple.of(id, config.name.value, snapshot?.version ?: 0))
+//            // Use the stream
+//            stream.exceptionHandler { err -> log.error("Stream error", err) }
+//            stream.handler { row ->
+//              val jsonObject: JsonObject = row.get(JsonObject::class.java, 0)
+//              val event: DomainEvent = config.json.decodeFromString(jsonObject.encode())
+//              currentVersion = row.getInteger(1)
+//              events.add(event as E)
+//              if (log.isDebugEnabled) log.debug("Event: $event version: $currentVersion")
+//            }
+//            stream.endHandler {
+//              if (log.isDebugEnabled) log.debug("End of stream")
+//              // Attempt to commit the transaction
+//              tx.commit { ar ->
+//                if (ar.failed()) {
+//                  log.error("tx.commit", ar.cause())
+//                  promise.fail(ar.cause())
+//                } else {
+//                  if (log.isDebugEnabled) log.debug("tx.commit successfully")
+//                  if (events.size == 0) {
+//                    promise.complete(snapshot)
+//                  } else {
+//                    val currentInstance = events.fold(
+//                      snapshot?.state,
+//                      { state, event -> config.eventHandler.handleEvent(state, event) }
+//                    )
+//                    promise.complete(Snapshot(currentInstance!!, currentVersion))
+//                  }
+//                }
+//              }
+//            }
+//          }
+//        }
+//      }
+//      return promise.future()
+//    }
 
     val promise = Promise.promise<Snapshot<A>>()
     writeModelDb.connection
@@ -154,8 +154,8 @@ class PgcSnapshotRepo<A : AggregateRoot, C : Command, E : DomainEvent>(
       }
       .onSuccess { conn: SqlConnection ->
         currentSnapshot(conn)
-          .compose { pair -> newSnapshot(pair.first, pair.second) }
-          .onSuccess { newSnapshot -> promise.complete(newSnapshot) }
+  //        .compose { pair -> newSnapshot(pair.first, pair.second) }
+          .onSuccess { newSnapshot -> promise.complete(newSnapshot.second) }
           .onFailure {
             log.error("When getting new snapshot", it)
             promise.fail(it)
