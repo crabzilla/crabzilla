0a2370aa70be399add01249a5eeacb4a277c0d48
rodolfodpk <rodolfo_dpk@yahoo.com>
2021-08-29 16:49:30 -0300

    cleanup

--- crabzilla-engine-postgres/src/main/java/io/github/crabzilla/engine/projector/EventsProjectorVerticle.kt
+++ crabzilla-engine-postgres/src/main/java/io/github/crabzilla/engine/projector/EventsProjectorVerticle.kt
@@ -1,10 +1,12 @@
 package io.github.crabzilla.engine.projector
 
+import io.github.crabzilla.core.serder.JsonSerDer
 import io.github.crabzilla.engine.PostgresAbstractVerticle
 import io.github.crabzilla.stack.EventRecord
 import io.vertx.core.Future
 import io.vertx.core.Promise
 import io.vertx.core.json.JsonObject
+import io.vertx.pgclient.PgPool
 import io.vertx.sqlclient.Tuple
 import org.slf4j.LoggerFactory
 import java.util.concurrent.atomic.AtomicLong
@@ -17,6 +19,8 @@ class EventsProjectorVerticle : PostgresAbstractVerticle() {
   companion object {
     private val log = LoggerFactory.getLogger(EventsProjectorVerticle::class.java)
     private const val defaultMetricInterval = 10_000L
+    private const val errorCode = 500
+    private const val expectedRows = 1
   }
 
   private lateinit var targetEndpoint: String
@@ -25,6 +29,45 @@ class EventsProjectorVerticle : PostgresAbstractVerticle() {
   private val eventSequence: AtomicLong = AtomicLong(0)
 
   override fun start(promise: Promise<Void>) {
+
+    fun consumers(eventsProjector: EventsProjector, serDer: JsonSerDer, pgPool: PgPool) {
+      vertx.eventBus().consumer<JsonObject>(targetEndpoint) { msg ->
+        val eventRecord = EventRecord.fromJsonObject(msg.body())
+        log.info(
+          "Event sequence {} current {}",
+          eventRecord.eventMetadata.eventSequence, eventSequence.get()
+        )
+        val event = serDer.eventFromJson(eventRecord.eventAsjJson.toString())
+        if (eventRecord.eventMetadata.eventSequence <= eventSequence.get()) {
+          log.debug(
+            "Ignoring event sequence {} since it's lower than {}",
+            eventRecord.eventMetadata.eventSequence, eventSequence.get()
+          )
+          msg.reply(true)
+          return@consumer
+        }
+        pgPool.withTransaction { conn ->
+          eventsProjector.project(conn, event, eventRecord.eventMetadata)
+            .compose { projectedSequence ->
+              log.debug("Projected {}", projectedSequence)
+              conn
+                .preparedQuery("update projections set sequence = $2 where name = $1 and sequence < $2")
+                .execute(Tuple.of(targetEndpoint, projectedSequence))
+            }
+        }
+          .onFailure {
+            msg.fail(errorCode, it.message)
+            failures.incrementAndGet()
+          }
+          .onSuccess {
+            msg.reply(true)
+            eventSequence.set(eventRecord.eventMetadata.eventSequence)
+          }
+      }
+      vertx.setPeriodic(config().getLong("metricsInterval", defaultMetricInterval)) {
+        publishMetrics()
+      }
+    }
 
     targetEndpoint = config().getString("targetEndpoint")
 
@@ -37,7 +80,7 @@ class EventsProjectorVerticle : PostgresAbstractVerticle() {
       conn.preparedQuery("select sequence from projections where name =$1")
         .execute(Tuple.of(targetEndpoint))
         .map { row ->
-          if (row.rowCount() == 1) {
+          if (row.rowCount() == expectedRows) {
             eventSequence.set(row.first().getLong("sequence"))
             Future.succeededFuture<Void>()
           } else {
@@ -49,42 +92,7 @@ class EventsProjectorVerticle : PostgresAbstractVerticle() {
         promise.fail(it)
       }
       .onSuccess {
-        vertx.eventBus().consumer<JsonObject>(targetEndpoint) { msg ->
-          val eventRecord = EventRecord.fromJsonObject(msg.body())
-          log.info(
-            "Event sequence {} current {}",
-            eventRecord.eventMetadata.eventSequence, eventSequence.get()
-          )
-          val event = serDer.eventFromJson(eventRecord.eventAsjJson.toString())
-          if (eventRecord.eventMetadata.eventSequence <= eventSequence.get()) {
-            log.debug(
-              "Ignoring event sequence {} since it's lower than {}",
-              eventRecord.eventMetadata.eventSequence, eventSequence.get()
-            )
-            msg.reply(true)
-            return@consumer
-          }
-          pgPool.withTransaction { conn ->
-            eventsProjector.project(conn, event, eventRecord.eventMetadata)
-              .compose { projectedSequence ->
-                log.debug("Projected {}", projectedSequence)
-                conn
-                  .preparedQuery("update projections set sequence = $2 where name = $1 and sequence < $2")
-                  .execute(Tuple.of(targetEndpoint, projectedSequence))
-              }
-          }
-            .onFailure {
-              msg.fail(500, it.message)
-              failures.incrementAndGet()
-            }
-            .onSuccess {
-              msg.reply(true)
-              eventSequence.set(eventRecord.eventMetadata.eventSequence)
-            }
-        }
-        vertx.setPeriodic(config().getLong("metricsInterval", defaultMetricInterval)) {
-          publishMetrics()
-        }
+        consumers(eventsProjector, serDer, pgPool)
         log.info("Started consuming from endpoint [{}]", targetEndpoint)
         promise.complete()
       }
