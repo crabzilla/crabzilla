4d50eddaec99cbea579f7d93a634c98c2425fba0
rodolfodpk <rodolfo_dpk@yahoo.com>
2021-09-01 10:54:38 -0300

    test

--- crabzilla-core/src/main/java/io/github/crabzilla/core/command/CommandException.kt
+++ crabzilla-core/src/main/java/io/github/crabzilla/core/command/CommandException.kt
@@ -3,5 +3,4 @@ package io.github.crabzilla.core.command
 sealed class CommandException(message: String) : Exception(message) {
   class ValidationException(errors: List<String>) : CommandException(errors.toString())
   class LockingException(message: String) : CommandException(message)
-//  class CommandHandlerException(e: Exception) : CommandException(e.message ?: e.toString())
 }
