21881758280da65f3f0e4b5abb876c974b2c5d1c
rodolfodpk <rodolfo_dpk@yahoo.com>
2021-11-12 01:01:41 -0300

    imports

--- crabzilla-core/src/main/java/io/github/crabzilla/core/metadata/EventMetadata.kt
+++ crabzilla-core/src/main/java/io/github/crabzilla/core/metadata/EventMetadata.kt
@@ -1,10 +1,15 @@
 package io.github.crabzilla.core.metadata
 
+import io.github.crabzilla.core.metadata.Metadata.CausationId
+import io.github.crabzilla.core.metadata.Metadata.CorrelationId
+import io.github.crabzilla.core.metadata.Metadata.EventId
+import io.github.crabzilla.core.metadata.Metadata.StateId
+
 data class EventMetadata(
   val aggregateName: String,
-  val stateId: Metadata.StateId,
-  val eventId: Metadata.EventId,
-  val correlationId: Metadata.CorrelationId,
-  val causationId: Metadata.CausationId,
+  val stateId: StateId,
+  val eventId: EventId,
+  val correlationId: CorrelationId,
+  val causationId: CausationId,
   val eventSequence: Long
 )
