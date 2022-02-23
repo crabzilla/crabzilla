eae87cf493634f7862267f14a409d5ec18604deb
rodolfodpk <rodolfo_dpk@yahoo.com>
2022-02-23 11:41:36 -0300

    renamed pgclient module

--- crabzilla-core/src/main/kotlin/io/github/crabzilla/core/EventTopics.kt
+++
@@ -1,6 +0,0 @@
-package io.github.crabzilla.core
-
-enum class EventTopics {
-  STATE_TOPIC,
-  VIEW_TOPIC
-}
--- crabzilla-vertx-pgclient/src/main/kotlin/io/github/crabzilla/pgclient/projection/EventsProjectorProvider.kt
+++
@@ -1,7 +0,0 @@
-package io.github.crabzilla.pgclient.projection
-
-import io.github.crabzilla.pgclient.EventsProjector
-
-interface EventsProjectorProvider {
-  fun create(): EventsProjector
-}
