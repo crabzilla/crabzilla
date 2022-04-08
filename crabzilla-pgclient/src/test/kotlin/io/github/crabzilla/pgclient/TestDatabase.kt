b54fe28087cf7a9bbf0f7bca8a9a2878201e6aa9
rodolfodpk <rodolfo_dpk@yahoo.com>
2022-04-08 17:37:01 -0300

    dropped snapshot impl kotlin serialization out of core back to java 11

--- crabzilla-pgclient/src/test/kotlin/io/github/crabzilla/pgclient/TestDatabase.kt
+++ crabzilla-pgclient/src/test/kotlin/io/github/crabzilla/pgclient/TestDatabase.kt
@@ -45,7 +45,6 @@ fun pgPool(vertx: Vertx): PgPool {
 fun cleanDatabase(sqlClient: SqlClient): Future<Void> {
   return sqlClient.query("delete from commands").execute()
     .compose { sqlClient.query("delete from events").execute() }
-    .compose { sqlClient.query("delete from snapshots").execute() }
     .compose { sqlClient.query("update projections set sequence = 0").execute() }
     .compose { sqlClient.query("alter sequence events_sequence_seq restart").execute() }
     .compose { sqlClient.query("delete from customer_summary").execute() }
