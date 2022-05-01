52cd5ef7265538da238220e043e8bb38f2654a7d
rodolfodpk <rodolfo_dpk@yahoo.com>
2022-05-01 20:10:50 -0300

    removind kotlinx serialization dependency from core test scope

--- crabzilla-core/src/test/kotlin/io/github/crabzilla/example1/customer/customer.kt
+++ crabzilla-core/src/test/kotlin/io/github/crabzilla/example1/customer/customer.kt
@@ -12,52 +12,27 @@ import io.github.crabzilla.example1.customer.CustomerCommand.RegisterCustomer
 import io.github.crabzilla.example1.customer.CustomerEvent.CustomerActivated
 import io.github.crabzilla.example1.customer.CustomerEvent.CustomerDeactivated
 import io.github.crabzilla.example1.customer.CustomerEvent.CustomerRegistered
-import kotlinx.serialization.Contextual
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
 import java.util.UUID
 
 /**
  * Customer events
  */
-@Serializable
 sealed class CustomerEvent {
-  @Serializable
-  @SerialName("CustomerRegistered")
-  data class CustomerRegistered(@Contextual val id: UUID, val name: String) : CustomerEvent()
-
-  @Serializable
-  @SerialName("CustomerActivated")
+  data class CustomerRegistered(val id: UUID, val name: String) : CustomerEvent()
   data class CustomerActivated(val reason: String) : CustomerEvent()
-
-  @Serializable
-  @SerialName("CustomerDeactivated")
   data class CustomerDeactivated(val reason: String) : CustomerEvent()
 }
 
 /**
  * Customer commands
  */
-@Serializable
 sealed class CustomerCommand {
-  @Serializable
   data class UnknownCommand(val x: String) : CustomerCommand()
-  @Serializable
-  @SerialName("RegisterCustomer")
-  data class RegisterCustomer(@Contextual val customerId: UUID, val name: String) : CustomerCommand()
-
-  @Serializable
-  @SerialName("ActivateCustomer")
+  data class RegisterCustomer(val customerId: UUID, val name: String) : CustomerCommand()
   data class ActivateCustomer(val reason: String) : CustomerCommand()
-
-  @Serializable
-  @SerialName("DeactivateCustomer")
   data class DeactivateCustomer(val reason: String) : CustomerCommand()
-
-  @Serializable
-  @SerialName("RegisterAndActivateCustomer")
   data class RegisterAndActivateCustomer(
-    @Contextual val customerId: UUID,
+    val customerId: UUID,
     val name: String,
     val reason: String
   ) : CustomerCommand()
@@ -66,15 +41,12 @@ sealed class CustomerCommand {
 /**
  * Customer aggregate root
  */
-@Serializable
-@SerialName("Customer")
 data class Customer(
-  @Contextual val id: UUID,
+  val id: UUID,
   val name: String,
   val isActive: Boolean = false,
   val reason: String? = null
 ) {
-
   companion object {
     fun create(id: UUID, name: String): List<CustomerEvent> {
       return listOf(CustomerRegistered(id = id, name = name))
@@ -83,11 +55,9 @@ data class Customer(
       return Customer(id = event.id, name = event.name, isActive = false)
     }
   }
-
   fun activate(reason: String): List<CustomerEvent> {
     return listOf(CustomerActivated(reason))
   }
-
   fun deactivate(reason: String): List<CustomerEvent> {
     return listOf(CustomerDeactivated(reason))
   }
