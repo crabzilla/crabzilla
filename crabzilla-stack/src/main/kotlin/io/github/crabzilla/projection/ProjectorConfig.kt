91196433ac6fead73a8716b8d3f69c6c61653c29
rodolfodpk <rodolfo_dpk@yahoo.com>
2022-05-01 16:51:59 -0300

    commented out ProjectorCOnfig json factory

--- crabzilla-stack/src/main/kotlin/io/github/crabzilla/projection/ProjectorConfig.kt
+++ crabzilla-stack/src/main/kotlin/io/github/crabzilla/projection/ProjectorConfig.kt
@@ -1,11 +1,5 @@
 package io.github.crabzilla.projection
 
-import io.github.crabzilla.projection.EventBusStrategy.EVENTBUS_PUBLISH
-import io.github.crabzilla.projection.EventBusStrategy.EVENTBUS_REQUEST_REPLY
-import io.github.crabzilla.projection.EventBusStrategy.EVENTBUS_REQUEST_REPLY_BLOCKING
-import io.vertx.core.json.JsonArray
-import io.vertx.core.json.JsonObject
-
 data class ProjectorConfig(
   val projectionName: String,
   val initialInterval: Long = DEFAULT_INITIAL_INTERVAL,
@@ -22,34 +16,34 @@ data class ProjectorConfig(
     private const val DEFAULT_INTERVAL = 5_000L
     private const val DEFAULT_NUMBER_ROWS = 250
     private const val DEFAULT_MAX_INTERVAL = 60_000L
-    fun create(config: JsonObject): ProjectorConfig {
-      val projectionName = config.getString("projectionName")
-      val initialInterval = config.getLong("initialInterval", DEFAULT_INITIAL_INTERVAL)
-      val interval = config.getLong("interval", DEFAULT_INTERVAL)
-      val maxNumberOfRows = config.getInteger("maxNumberOfRows", DEFAULT_NUMBER_ROWS)
-      val maxInterval = config.getLong("maxInterval", DEFAULT_INTERVAL)
-      val metricsInterval = config.getLong("metricsInterval", DEFAULT_MAX_INTERVAL)
-      val stateTypesArray = config.getJsonArray("stateTypes") ?: JsonArray()
-      val stateTypes = stateTypesArray.iterator().asSequence().map { it.toString() }.toList()
-      val eventTypesArray = config.getJsonArray("eventTypes") ?: JsonArray()
-      val eventTypes = eventTypesArray.iterator().asSequence().map { it.toString() }.toList()
-      val projectorStrategy = when (config.getString("eventBusStrategy")) {
-        "EVENTBUS_REQUEST_REPLY" -> EVENTBUS_REQUEST_REPLY
-        "EVENTBUS_REQUEST_REPLY_BLOCKING" -> EVENTBUS_REQUEST_REPLY_BLOCKING
-        "EVENTBUS_PUBLISH" -> EVENTBUS_PUBLISH
-        else -> throw IllegalArgumentException("Invalid config for eventBusStrategy")
-      }
-      return ProjectorConfig(
-        projectionName = projectionName,
-        initialInterval = initialInterval,
-        interval = interval,
-        maxInterval = maxInterval,
-        metricsInterval = metricsInterval,
-        maxNumberOfRows = maxNumberOfRows,
-        stateTypes = stateTypes,
-        eventTypes = eventTypes,
-        eventBusStrategy = projectorStrategy
-      )
-    }
+//    fun create(config: JsonObject): ProjectorConfig {
+//      val projectionName = config.getString("projectionName")
+//      val initialInterval = config.getLong("initialInterval", DEFAULT_INITIAL_INTERVAL)
+//      val interval = config.getLong("interval", DEFAULT_INTERVAL)
+//      val maxNumberOfRows = config.getInteger("maxNumberOfRows", DEFAULT_NUMBER_ROWS)
+//      val maxInterval = config.getLong("maxInterval", DEFAULT_INTERVAL)
+//      val metricsInterval = config.getLong("metricsInterval", DEFAULT_MAX_INTERVAL)
+//      val stateTypesArray = config.getJsonArray("stateTypes") ?: JsonArray()
+//      val stateTypes = stateTypesArray.iterator().asSequence().map { it.toString() }.toList()
+//      val eventTypesArray = config.getJsonArray("eventTypes") ?: JsonArray()
+//      val eventTypes = eventTypesArray.iterator().asSequence().map { it.toString() }.toList()
+//      val projectorStrategy = when (config.getString("eventBusStrategy")) {
+//        "EVENTBUS_REQUEST_REPLY" -> EVENTBUS_REQUEST_REPLY
+//        "EVENTBUS_REQUEST_REPLY_BLOCKING" -> EVENTBUS_REQUEST_REPLY_BLOCKING
+//        "EVENTBUS_PUBLISH" -> EVENTBUS_PUBLISH
+//        else -> throw IllegalArgumentException("Invalid config for eventBusStrategy")
+//      }
+//      return ProjectorConfig(
+//        projectionName = projectionName,
+//        initialInterval = initialInterval,
+//        interval = interval,
+//        maxInterval = maxInterval,
+//        metricsInterval = metricsInterval,
+//        maxNumberOfRows = maxNumberOfRows,
+//        stateTypes = stateTypes,
+//        eventTypes = eventTypes,
+//        eventBusStrategy = projectorStrategy
+//      )
+//    }
   }
 }
